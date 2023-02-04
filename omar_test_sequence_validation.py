632ac68d0959b1877d72965e2607661544748db5
Omar Elsherif <omarelsherif010@gmail.com>
2023-02-04 12:55:02 +0200

    test_validation_rules_sequence

--- omar_test_sequence_validation.py
+++ omar_test_sequence_validation.py
@@ -2,9 +2,13 @@ import sbol3
 
 sbol3.set_namespace('http://sbolstandard.org/testfiles')
 
-gfp = sbol3.Component('GFP', sbol3.SBO_DNA)
-gfp_seq = sbol3.Sequence('GFPSequence', elements='atgnnntaa', encoding=sbol3.IUPAC)
+gfp_seq1 = sbol3.Sequence('GFPSequence', elements='atgnnntaa', encoding=sbol3.IUPAC_DNA_ENCODING)
+gfp_seq2 = sbol3.Sequence('GFPSequence', elements='atgnnntaa', encoding='https://identifiers.org/edam:format_1207')
+gfp_seq3 = sbol3.Sequence('GFPSequence', elements='atgnnntaa', encoding='DNA')
+gfp_seq4 = sbol3.Sequence('GFPSequence', elements='atgnnntaa')
 
-gfp.sequences = [ gfp_seq ]
 
-print(gfp.sequences)
+print(f'gfp_seq1 -> {gfp_seq1.validate()}')
+print(f'gfp_seq2 -> {gfp_seq2.validate()}')
+print(f'gfp_seq3 -> {gfp_seq3.validate()}')
+print(f'gfp_seq4 -> {gfp_seq4.validate()}')
